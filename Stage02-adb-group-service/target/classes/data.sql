68971381fa69c37672cd3998c00a3be89f50e849
pcbuilderayush <pcbuilderayush@gmail.com>
2020-02-28 15:17:37 +0530

    Add files via upload

---
+++ Stage02-adb-group-service/target/classes/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO groups
+VALUES('FAMILY','FAMILY Members','All my Family members');
+INSERT INTO groups
+VALUES('WORK','My Work Mates','All my Work Mates');
+INSERT INTO groups
+VALUES('Gym','Gym Friends','All my Gym Friends');
